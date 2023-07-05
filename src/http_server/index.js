2d8b8e8a3b4dbff29e072e3ccc071ce768045cda
Ekalape <ekalape@outlook.it>
2023-07-05 10:25:52 +0200

    feat: add websocket to server

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,17 +1,38 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
+import { wsRequestHandler } from '../websocket/handler.js';
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  let response;
+  ws.on('error', console.error);
+
+  ws.on('open', () => {
+    console.log('open');
+  });
+
+  ws.on('message', function message(data) {
+    response = wsRequestHandler(data);
+    ws.send(response);
+  });
+
+  ws.send('something');
 });
